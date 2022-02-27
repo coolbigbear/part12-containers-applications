5c9e99c213e478e4e881468b99f4c67af37ab404
coolbigbear <mrczuper@gmail.com>
2022-02-27 15:30:48 +0200

    Done up to and including 12.16

--- todo-app/todo-frontend/src/App.test.js
+++ todo-app/todo-frontend/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Todo from './Todos/Todo';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Todos/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('single todo is correctly displayed', () => {
+  render(<Todo todo={{ text: "test todo", done: false }} deleteTodo={() => { }} completeTodo={() => { }}/>);
+  const linkElement = screen.getByText(/test todo/i);
   expect(linkElement).toBeInTheDocument();
 });
